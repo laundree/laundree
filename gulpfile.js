0cf110d3d7e3565ff27503c25da76ab7a7bb39ea
Christian Budde Christensen <budde377@gmail.com>
2016-02-28 12:56:00 +0100

    Added initial project

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+var eslint = require('gulp-eslint')
+
+gulp.task('lint', function () {
+  return gulp.src(['**/*.js', 'gulpfile.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
