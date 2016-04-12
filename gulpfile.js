0a3c48176078a703a1a198e8c328896a8a50b24e
Christian Budde Christensen <budde377@gmail.com>
2016-04-12 21:11:34 +0200

    feat: Add initial messaging
    Add initial messaging via Redis.
    Setup with environment variables:
    
        REDIS_HOST
        REDIS_PORT

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp')
 var eslint = require('gulp-eslint')
+var fs = require('fs')
+var browserify = require('browserify')
+var babelify = require('babelify')
 
 gulp.task('lint', function () {
   return gulp.src(['**/*.js', 'gulpfile.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+})
+
+gulp.task('build', function () {
+  return browserify('./client/index.js')
+    .transform(babelify, {presets: ['es2015']})
+    .bundle()
+    .pipe(fs.createWriteStream('resources/bundle.js'))
 })
