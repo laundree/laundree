6eb86136e69a4569f9d80f85e2a567d5dc0a78d9
Christian Budde Christensen <budde377@gmail.com>
2016-04-29 07:56:10 +0200

    fix: Send coverage from docker
    Send coverage from docker instance.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var mocha = require('gulp-mocha')
 var istanbul = require('gulp-istanbul')
 var isparta = require('isparta')
 var mongoose = require('mongoose')
+var exec = require('child_process').exec
+var runSequence = require('run-sequence')
 
 gulp.task('lint', function () {
   return gulp.src(['{client,handlers,models,routes,setups,tests,utils,views}/**/*.js', '*.js'])
@@ -12,7 +14,7 @@ gulp.task('lint', function () {
     .pipe(eslint.failAfterError())
 })
 
-gulp.task('test-unit', function () {
+gulp.task('test-unit', function (done) {
   gulp.src(['{handlers,models,utils}/**/*.js'])
     .pipe(istanbul({ // Covering files
       instrumenter: isparta.Instrumenter,
@@ -27,9 +29,30 @@ gulp.task('test-unit', function () {
           reportOpts: {dir: 'coverage'},
           reporters: ['text', 'text-summary', 'lcov']
         }))
-        .on('end', () => mongoose.connection.close())
-        .on('error', () => mongoose.connection.close())
+        .on('end', () => {
+          mongoose.connection.close()
+          done()
+        })
+        .on('error', (error) => {
+          mongoose.connection.close()
+          done(error)
+        })
     })
 })
 
-gulp.task('test', ['lint', 'test-unit'], (done) => done())
+gulp.task('send-coverage', (done) => {
+  if (!process.env.CODECLIMATE_REPO_TOKEN) return done()
+  gulp.on('finish', () => {
+    console.log('FIN')
+    exec('./node_modules/.bin/codeclimate-test-reporter < coverage/lcov.info',
+      (err, stdout, stderr) => {
+        console.log(stdout)
+        console.log(stderr)
+        done(err)
+      })
+  })
+})
+
+gulp.task('test', (done) => {
+  runSequence('lint', 'test-unit', 'send-coverage', done)
+})
