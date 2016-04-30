e3724ea02bc551c502c03f0673ca3c3abe9fbeb0
Christian Budde Christensen <budde377@gmail.com>
2016-04-30 08:54:50 +0200

    fix: Increase test timeout
    Increase test timeout from 5s to 10s.

--- tests/unit/handlers/user.spec.js
+++ tests/unit/handlers/user.spec.js
@@ -10,7 +10,7 @@ var clearDb = require('../../db_utils').clearDb
 var UserHandler = require('../../../handlers').UserHandler
 
 describe('UserHandler', function () {
-  this.timeout(5000)
+  this.timeout(10000)
 //  var userHandler
   var profile = {
     provider: 'facebook',
