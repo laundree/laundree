38052c5f56395c3f60692879644bc4a28336177d
Christian Budde Christensen <budde377@gmail.com>
2016-05-13 20:32:58 +0200

    feat: New identicon provider
    Using jdenticon and svgs.

--- routes/identicon.js
+++ routes/identicon.js
@@ -2,23 +2,22 @@
  * Created by budde on 13/05/16.
  */
 
-var identicon = require('identicon')
+var jdenticon = require('jdenticon')
 var express = require('express')
-
+var crypto = require('crypto')
 var router = express.Router()
 
-router.get('/:id/:size.png', (req, res, next) => {
+router.get('/:id/:size.svg', (req, res, next) => {
   var size = parseInt(req.params.size)
   if (isNaN(size) || size <= 0) {
     var error = new Error('Image not found')
     error.status = 404
     return next(error)
   }
-  identicon.generate({id: req.params.id, size: size}, (err, buffer) => {
-    if (err) return next(err)
-    res.set('Content-Type', 'image/png')
-    res.send(buffer)
-  })
+  res.set('Content-Type', 'image/svg+xml')
+  var hash = crypto.createHash('md5').update(req.params.id).digest('hex')
+  var svg = jdenticon.toSvg(hash, size)
+  res.send(svg)
 })
 
 module.exports = router
