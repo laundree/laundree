bd71da28543804112086fe64d25b0d2a0d3b58b3
Christian Budde Christensen <budde377@gmail.com>
2016-06-12 20:25:41 +0200

    fix: Reduce code dup

--- redux/index.js
+++ redux/index.js
@@ -4,6 +4,13 @@
 const {createStore} = require('redux')
 const reducer = require('./reducer')
 const actions = require('./actions')
+
+function dispatchFlashs (store, flashArray, type) {
+  flashArray
+    .map((message) => ({type: type, message: message}))
+    .forEach((flash) => store.dispatch(actions.flash(flash)))
+}
+
 /**
  * Create initial store
  * @param {UserHandler} user
@@ -12,12 +19,8 @@ const actions = require('./actions')
 function createInitialStore (user, successFlash = [], errorFlash = []) {
   const store = createStore(reducer)
   if (user) store.dispatch(actions.signInUser(user))
-  successFlash
-    .map((message) => ({type: 'success', message: message}))
-    .forEach((flash) => store.dispatch(actions.flash(flash)))
-  errorFlash
-    .map((message) => ({type: 'error', message: message}))
-    .forEach((flash) => store.dispatch(actions.flash(flash)))
+  dispatchFlashs(store, successFlash, 'success')
+  dispatchFlashs(store, errorFlash, 'error')
   return Promise.resolve(store)
 }
 
