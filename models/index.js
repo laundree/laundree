6a36765fee4b0ed1526ed412adbe2559f174a597
Christian Budde Christensen <budde377@gmail.com>
2016-09-11 12:32:21 +0200

    fix: Reconnect mongoose
    Closes #221

--- models/index.js
+++ models/index.js
@@ -2,11 +2,22 @@
  * Created by budde on 27/04/16.
  */
 
-var mongoose = require('mongoose')
-var config = require('config')
+const mongoose = require('mongoose')
+const config = require('config')
+const debug = require('debug')('laundree.models')
 
 mongoose.connect(config.get('mongo.url'))
 mongoose.Promise = require('promise')
+
+mongoose.connection.on('connected', () => debug('Mongoose connected'))
+
+mongoose.connection.on('error', (err) => {
+  debug('Mongoose connection erred: ', err)
+  debug('Restarting in a moment...')
+  setTimeout(() => mongoose.connect(config.get('mongo.url')), 1000)
+})
+
+mongoose.connection.on('disconnected', () => debug('Mongoose disconnected...'))
 
 module.exports = {
   UserModel: require('./user'),
