6d0bb4b15d20ac0723e6fe1450a0885656555f6f
Christian Budde Christensen <budde377@gmail.com>
2016-09-11 15:22:51 +0200

    feat Integrate with opbeat
    Closes #50, closes #198

--- utils/error.js
+++ utils/error.js
@@ -1,8 +1,11 @@
 const config = require('config')
+const opbeat = require('../lib/opbeat')
 
 function logError (err) {
   if (!config.get('logging.error.enabled')) return
   console.error(err)
+  if (!opbeat) return
+  opbeat.captureError(err)
 }
 
 module.exports = {logError: logError}
