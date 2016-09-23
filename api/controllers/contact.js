05daf6fcf8c52bcc635e2136d87e49f6c877f9f9
Christian Budde Christensen <budde377@gmail.com>
2016-09-23 23:21:07 +0200

    Update contact.js

--- api/controllers/contact.js
+++ api/controllers/contact.js
@@ -6,22 +6,20 @@ const config = require('config')
 
 function contact (req, res) {
   const {message, subject, email, name} = req.swagger.params.body.value
-  var template, receiver, sender, userId
+  var template, receiver, userId
   const user = req.user
   if (user) {
-    sender = `"${user.model.displayName}" <${user.model.email}>`
     template = 'support'
     receiver = config.get('emails.support')
     userId = user.model.id
   } else {
     if (!name) return api.returnError(res, 400, 'Name is required')
     if (!email) return api.returnError(res, 400, 'E-mail is required')
-    sender = `"${name}" <${email}>`
     template = 'contact'
     receiver = config.get('emails.contact')
   }
-  sendEmail({message, subject, email, name, userId}, template, receiver, sender)
-    .then(sendEmail({message, subject, name}, 'contact-receipt', sender))
+  sendEmail({message, subject, email, name, userId}, template, receiver)
+    .then(() => sendEmail({message, subject, name}, 'contact-receipt', sender))
     .then(() => api.returnSuccess(res))
     .catch(api.generateErrorHandler(res))
 }
