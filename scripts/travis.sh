6a9afd02e5adb7b86440140cb50999804c6c5b8e
Christian Budde Christensen <budde377@gmail.com>
2016-10-26 17:58:39 +0200

    feat: Build with travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     docker-compose -f docker-compose.test.yml run -e CODECLIMATE_REPO_TOKEN=$CODECLIMATE_REPO_TOKEN sut
 else
