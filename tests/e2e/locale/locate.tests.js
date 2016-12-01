6f5fb7b1ddbea34e8f4deec127d5d5f31ec62b21
Christian Budde Christensen <budde377@gmail.com>
2016-12-01 20:43:47 +0100

    feat: Add tests

---
+++ tests/e2e/locale/locate.tests.js
@@ -0,0 +1,31 @@
+/**
+ * Created by budde on 01/12/2016.
+ */
+const {timeout} = require('../../nightwatch_utils.js')
+
+module.exports = {
+  'Can change locale on home page': client => {
+    client
+      .url(client.launch_url)
+      .waitForElementPresent('#TopNav .language .dropDownTitle', timeout)
+      .click('#TopNav .language .dropDownTitle')
+      .waitForElementVisible('#TopNav .language .dropDownContent', timeout)
+      .click('#TopNav .language .dropDownContent li:nth-of-type(2) a')
+      .waitForElementVisible('#Home header h2', timeout)
+      .expect.element('#Home header h2').text.to.contain('Et nemt og gratis booking system til dit vasketøj')
+    client.end()
+  },
+  'Can change locale on auth': client => {
+    client
+      .url(client.launch_url)
+      .waitForElementPresent('#TopNav a.log-in', timeout)
+      .click('#TopNav a.log-in')
+      .waitForElementPresent('#AuthNav .language .dropDownTitle', timeout)
+      .click('#AuthNav .language .dropDownTitle')
+      .waitForElementVisible('#AuthNav .language .dropDownContent', timeout)
+      .click('#AuthNav .language .dropDownContent li:nth-of-type(2) a')
+      .waitForElementVisible('#Auth h1', timeout)
+      .expect.element('#Auth h1').text.to.contain('Log ind')
+    client.end()
+  }
+}
