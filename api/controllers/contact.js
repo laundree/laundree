b3c9a5521fa7f650f8e0dd9198ffd4958cd4e2d9
Christian Budde Christensen <budde377@gmail.com>
2016-12-10 11:00:48 +0100

    fix: Mail variables

--- api/controllers/contact.js
+++ api/controllers/contact.js
@@ -6,10 +6,11 @@ const config = require('config')
 
 function contact (req, res) {
   const {message, subject, email, name} = req.swagger.params.body.value
-  let template, receiver, sender, userId, senderEmail
+  let template, receiver, sender, userId, senderEmail, senderName
   const user = req.user
   if (user) {
     senderEmail = user.model.emails[0]
+    senderName = user.model.displayName
     sender = `"${user.model.displayName}" <${senderEmail}>`
     template = 'support'
     receiver = config.get('emails.support')
@@ -17,12 +18,13 @@ function contact (req, res) {
   } else {
     if (!name) return api.returnError(res, 400, 'Name is required')
     if (!email) return api.returnError(res, 400, 'E-mail is required')
+    senderName = name
     senderEmail = email
     sender = `"${name}" <${email}>`
     template = 'contact'
     receiver = config.get('emails.contact')
   }
-  sendEmail({message, subject, email: senderEmail, name, userId}, template, receiver, {locale: req.locale})
+  sendEmail({message, subject, email: senderEmail, name: senderName, userId}, template, receiver, {locale: req.locale})
     .then(() => sendEmail({message, subject, name}, 'contact-receipt', sender, {locale: req.locale}))
     .then(() => api.returnSuccess(res))
     .catch(api.generateErrorHandler(res))
