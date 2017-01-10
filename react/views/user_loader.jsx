e53724b6978b2855fbe163edb3f3273650cef6b8
Christian Budde Christensen <budde377@gmail.com>
2017-01-10 16:35:47 +0100

    fix: UserLoader is now loading laundries

--- react/views/user_loader.jsx
+++ react/views/user_loader.jsx
@@ -2,13 +2,20 @@ const React = require('react')
 const Loader = require('./loader.jsx')
 const sdk = require('../../client/sdk')
 
-const UserLoader = ({userId, children, users}) => <Loader
-  loader={() => sdk.fetchUser(userId)}
-  loaded={users[userId]}>{children}</Loader>
+function hasLaundries (laundries, user) {
+  return user.laundries.every(id => laundries[id])
+}
 
+const UserLoader = ({userId, children, users, laundries}) => {
+  const user = users[userId]
+  return <Loader
+    loader={() => sdk.fetchUser(userId)}
+    loaded={user && hasLaundries(laundries, user)}>{children}</Loader>
+}
 UserLoader.propTypes = {
   children: React.PropTypes.any,
   users: React.PropTypes.object.isRequired,
+  laundries: React.PropTypes.object.isRequired,
   userId: React.PropTypes.string.isRequired
 }
 
