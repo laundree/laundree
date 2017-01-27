fd43533fd21bfd8f35fe28a691028f5665c1f651
Christian Budde Christensen <budde377@gmail.com>
2017-01-27 07:49:47 +0100

    hack: Rename

--- react/containers/AdminPanel.js
+++ react/containers/AdminPanel.js
@@ -3,7 +3,7 @@
  */
 
 const connect = require('react-redux').connect
-const AdminPanel = require('../views/admin_panel.jsx')
+const AdminPanel = require('../views/AdminPanel')
 
 const mapStateToProps = ({users, userList, userListSize, stats, currentUser, laundries, laundryList, laundryListSize}) => {
   return {user: users[currentUser], stats, laundries, users, userList, userListSize, laundryList, laundryListSize}
