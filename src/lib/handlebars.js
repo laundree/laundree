a059b299e864ef71ba8674cf8c199d4abc8ccc91
Christian Budde Christensen <budde377@gmail.com>
2017-04-30 15:13:29 +0200

    fix: Add missing files

---
+++ src/lib/handlebars.js
@@ -0,0 +1,45 @@
+const handlebars = require('handlebars')
+const fs = require('../utils/fs')
+const path = require('path')
+const hb = require('../utils/handlebars')
+const partialsPath = path.resolve(__dirname, '..', '..', 'templates', 'partials')
+const debug = require('debug')('laundree.lib.handlebars')
+
+function setupRenderHb (req, res, next) {
+  res.renderHb = (file, args) => {
+    const locale = req.locale
+    hb
+      .render(path.join('web', file),
+        args,
+        locale)
+      .then(result => res.send(result), next)
+  }
+  next()
+}
+function format (context, options) {
+  return new handlebars.SafeString(Object.keys(options.hash).reduce((message, key) => {
+    const regexp = new RegExp(`{${key}}`, 'g')
+    return message.replace(regexp, options.hash[key])
+  }, context))
+}
+
+function setup (app) {
+  app.use(setupRenderHb)
+  handlebars.registerHelper({
+    formatIntl: (context, options) => {
+      const messages = options.hash.messages
+      const message = messages[context]
+      return format(message, options)
+    },
+    format
+  })
+  return fs.readdir(partialsPath)
+    .then(files => Promise.all(files.map(file => fs
+      .readFile(path.resolve(partialsPath, file), 'utf8')
+      .then(data => {
+        debug(`Registered partial ${file}`)
+        return handlebars.registerPartial(file, handlebars.compile(data))
+      }))))
+}
+
+module.exports = setup
---
+++ src/lib/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by budde on 26/04/16.
+ */
+
+module.exports = {
+  passportSetup: require('./passport'),
+  socketIoSetup: require('./socket_io'),
+  sessionSetup: require('./session'),
+  morganSetup: require('./morgan'),
+  defaultUserSetup: require('./default_users'),
+  handlebarsSetup: require('./handlebars')
+}
