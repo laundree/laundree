0a85754cf5c434727dc2bbbf544bad1af59fe33c
Christian Budde Christensen <budde377@gmail.com>
2017-04-30 17:41:24 +0200

    fix: Move coverage reporting

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,15 +11,23 @@ set -e
 
 npm run setup:test
 
-
 npm run test:unit-covered
+
+nyc report
+
+if [ -z "$CODECLIMATE_REPO_TOKEN" ]; then
+    echo "No code climate token defined. Not uploading coverage."
+else
+    codeclimate-test-reporter < coverage/lcov.info
+fi
+
 
 if [ -z "$SELENIUM_HOST" ] || [ "$SELENIUM_HOST" = "localhost" ]; then
     selenium-standalone install
     selenium-standalone start 2>/dev/null >/dev/null &
 fi
 
-npm run start:test-covered &
+npm run start:test &
 
 until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
     printf '.'
@@ -29,13 +37,6 @@ sleep 5
 
 ./scripts/run-nightwatch.js
 
-nyc report
-
-if [ -z "$CODECLIMATE_REPO_TOKEN" ]; then
-    echo "No code climate token defined. Not uploading coverage."
-else
-    codeclimate-test-reporter < coverage/lcov.info
-fi
 
 echo "Exiting..."
 
