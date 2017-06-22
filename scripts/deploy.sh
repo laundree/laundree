b3f698d5735a2100be948eb31bd412a73f326ab7
Christian Budde Christensen <budde377@gmail.com>
2017-06-22 17:07:30 +0200

    feat: Add deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,9 @@ set -e
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker build -t laundree/laundree:${TRAVIS_COMMIT} --build-arg NODE_ENV=production .
 docker push laundree/laundree:${TRAVIS_COMMIT}
+
+BRANCH=${TRAVIS_BRANCH} TAG=${TRAVIS_COMMIT} envsub < kube/laundree.yml > laundree-deployment.yml
+
+~/google-cloud-sdk/bin/gcloud container clusters get-credentials cluster-1 \
+    --zone europe-west1-b --project laundree-io
+~/google-cloud-sdk/bin/kubectl apply -f laundree-deployment.yml
