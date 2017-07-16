65b18cd7bad99b5a6a8a9d24c26545b993b8a80d
Christian Budde Christensen <budde377@gmail.com>
2017-07-16 13:12:00 +0200

    feat: Upgrade npm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ set -e
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker build -t laundree/laundree:${TRAVIS_COMMIT} --build-arg NODE_ENV=production .
-docker tag laundree/laundree:${TRAVIS_COMMIT} laundree/laundree:latest
+docker tag laundree/laundree:${TRAVIS_COMMIT} laundree/laundree:${TRAVIS_BRANCH}
 docker push laundree/laundree:${TRAVIS_COMMIT}
-docker push laundree/laundree:latest
+docker push laundree/laundree:${TRAVIS_BRANCH}
 
 BRANCH=${TRAVIS_BRANCH} TAG=${TRAVIS_COMMIT} envsubst < kube/laundree.yml > laundree-deployment.yml
 
