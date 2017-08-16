a2f2b70a28658221044dd19d86df8f60dd9dfbd2
Christian Budde Christensen <budde377@gmail.com>
2017-08-16 21:42:25 +0200

    feat: Move tos to external

--- src/react/views/App.js
+++ src/react/views/App.js
@@ -8,8 +8,7 @@ import Auth from '../containers/Auth'
 import BaseApp from '../containers/BaseApp'
 import NativeApp from '../containers/NativeApp'
 import NativeAppV2 from '../containers/NativeAppV2'
-import TermsAndConditions from '../views/TermsAndConditions'
-import Privacy from '../views/Privacy'
+import TermsAndConditions from '../containers/TermsAndConditions'
 import StateCheckRedirectRoute from '../containers/StateCheckRedirectRoute'
 import GAWrapper from '../containers/GAWrapper'
 import type {LocaleType} from './../../locales'
@@ -25,7 +24,7 @@ const App = (props: {locale: LocaleType, store: Store}) => {
               test={({currentUser}) => !currentUser}
               path='/auth'
               component={Auth} />
-            <Route path='/privacy' component={Privacy} />
+            <Route path='/privacy' component={TermsAndConditions} />
             <Route path='/terms-and-conditions' component={TermsAndConditions} />
             <Route path='/native-app' component={NativeApp} />
             <Route path='/native-app-v2' component={NativeAppV2} />
