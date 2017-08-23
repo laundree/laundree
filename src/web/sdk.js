8a7be081d3e97b19a1d5124e3abbf5ebbbff6735
Christian Budde Christensen <budde377@gmail.com>
2017-08-24 01:05:16 +0200

    fix: Token cache bug

--- src/web/sdk.js
+++ src/web/sdk.js
@@ -3,13 +3,19 @@
 import { Sdk } from 'laundree-sdk'
 import { signAppToken } from '../auth'
 import config from 'config'
+import Debug from 'debug'
+
+const debug = Debug('laundree.web.sdk')
 
 let tokenCache: ?{token: string, cacheTime: number} = null
 
 const authenticator = async () => {
-  if (tokenCache && tokenCache.cacheTime + 60 * 60 * 1000 < Date.now()) {
+  debug('Authenticating')
+  if (tokenCache && (tokenCache.cacheTime + 60 * 60 * 1000) > Date.now()) {
+    debug('Fetching token from cache')
     return {type: 'bearer', token: tokenCache.token}
   }
+  debug('Generating new token')
   tokenCache = {token: await signAppToken('https://web.laundree.io', 'https://api.laundree.io'), cacheTime: Date.now()}
   return {type: 'bearer', token: tokenCache.token}
 }
