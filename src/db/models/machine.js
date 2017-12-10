7f56a682cf801ee3de527f7a4bf206144ec9a53c
Christian Budde Christensen <budde377@gmail.com>
2017-12-10 16:05:44 +0100

    fix: Mongoose

--- src/db/models/machine.js
+++ src/db/models/machine.js
@@ -1,6 +1,7 @@
 // @flow
 import mongoose from 'mongoose'
-import type {ObjectId} from 'mongoose'
+import type { ObjectId } from 'mongoose'
+
 const {Schema} = mongoose
 
 export type MachineType = 'wash' | 'dry'
@@ -17,7 +18,7 @@ const machineSchema: Schema<MachineDescription> = new Schema({
   type: {type: String, enum: ['wash', 'dry'], required: true},
   laundry: {type: Schema.Types.ObjectId, ref: 'Laundry', required: true},
   broken: {type: Boolean, required: true, default: false}
-}, {timestamps: true})
+}, {timestamps: true, usePushEach: true})
 
 machineSchema.index({name: 1, laundry: 1}, {unique: true})
 machineSchema.index({'from': 1})
